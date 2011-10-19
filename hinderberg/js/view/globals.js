6605d41eeebe612a8b3a4814fb9c1aac5ed0fc2c
Hans Magnus Inderberg <hinderberg@gmail.com>
2011-10-19 17:01:26 +0200

    added mucho stuff

--- hinderberg/js/view/globals.js
+++ hinderberg/js/view/globals.js
@@ -3,11 +3,11 @@
 		this.touchEvent = ('createTouch' in document) ? 'touchstart' : 'click';	
 		
 		function allowFormsInIscroll(){
-	  	[].slice.call(document.querySelectorAll('input, select, button')).forEach(function(el){
-	    	el.addEventListener(('ontouchstart' in window)?'touchstart':'mousedown', function(e){
-	      	e.stopPropagation();
-	    	});
-	  	});
+		  	[].slice.call(document.querySelectorAll('input, select, button')).forEach(function(el){
+		    	el.addEventListener(('ontouchstart' in window)?'touchstart':'mousedown', function(e){
+		      	e.stopPropagation();
+		    	});
+		  	});
 		}		
 		
 		allowFormsInIscroll();

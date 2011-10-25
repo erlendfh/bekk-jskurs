d2b672c844ee70f3413df50587d6459f7b4aedc1
Hans Magnus Inderberg <hinderberg@gmail.com>
2011-10-25 19:09:02 +0200

    To much work without a commit! Note to self: Commit more often!
    Repeat after me: Commit more often! Commit more often! Commit more
    often! Commit more often! Commit more often! Commit more often! Commit
    more often! Commit more often! Commit more often! Commit more often!

--- hinderberg/js/view/tasks.js
+++ hinderberg/js/view/tasks.js
@@ -12,55 +12,73 @@
     this.inputField = $(params.inputField);
     this.taskListElement = $(params.taskListElement);
 
-    this.taskListElement.append(this.renderTasksHTML());
+    this.render();
 
     this.initializeBindings();
   }
 
   $.extend(Tasks.prototype, {
     addTask: function(task) {
-      this.persistence.create(task.values);
-      this.taskListElement.append(this.renderTaskHTML(task));
+      var tl = this.taskListElement;
+      this.persistence.create(task);
+      var el = $(this.taskHTML(task));
+      tl.prepend(el);
+      var offset = el.height();
+      tl.css("top", (-offset) + "px");
+      tl.anim({translate3d: '0px, ' + offset  + 'px, 0px'}, 0.5, 'ease-in-out 1ms', function () {
+        tl.attr('style', '');
+      });
     },
 
     removeTask: function(taskId) {
       this.persistence.destroy(taskId);
     },
 
-    renderTasksHTML: function() {
+    render: function() {
       var html = "";
       var that = this;
       this.persistence.findAll().forEach(function(task) {
-        
-        html += that.renderTaskHTML(task);
+        html += that.taskHTML(task);
       });
 
-      return html;
+      this.taskListElement.html(html);
     },
 
-    renderTaskHTML: function(task) {
+    taskHTML: function(task) {
       var checked = task.done ? 'checked' : "";
       return '<li value=' + task.id + '><input type="checkbox" name="done" ' + checked +  ' value="1" />' + task.text + '</li>';
     },
 
     initializeBindings: function () {
       var that = this;
+      console.log(this.inputField);
       this.inputField.bind('blur', function(event) {
-        that.addTask(new Task({
+        that.addTask({
           text: event.srcElement.value,
           done: false,
           date: {}
-        }));
+        });
 
         event.srcElement.value = '';
       });
 
-      this.taskListElement.delegate('input', reminders.globals.touchEvent, function() {
+      this.taskListElement.delegate('input', 'change', function() {
         var checkbox = $(this);
         var checked = checkbox.is(':checked');
         var task = that.persistence.find(checkbox.parent().attr('value'));
         task.done = checked;
         that.persistence.update(task);
+      });
+
+      this.taskListElement.delegate('li', 'swipeRight', function() {
+        var listElement = $(this);
+
+        listElement.anim({
+          translate3d: listElement.width() + 'px, 0px, 0px'
+          }, 0.5, 'ease-in-out 1ms', function() {
+            that.persistence.destroy(listElement.attr('value'));
+            that.render(); 
+          });
       });
     }
   });
